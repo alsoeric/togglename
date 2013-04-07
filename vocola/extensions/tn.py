4b3d9bdd86fb1cc84f42d30d63d4a85ae66f6254
Eric S. Johansson <ynotlayabout@gmail.com>
2013-04-07 16:24:29 -0400

    initial commit

---
+++ vocola/extensions/tn.py
@@ -0,0 +1,510 @@
+"""process
+    get region
+    Once we have region, fragment into components, one component at a time.
+    components are:
+        stringname
+        codename
+        bangbangname
+        notname #garbange can, whitespace, codelitmeter + blah 
+        
+        
+        
+        
+    predicats are :
+        (is stringname) and (not cursor needed )and (string_to_code)
+        (is stringname )and (cursor needed) and (string_to_code)
+        
+        (is codename )and (not cursor needed) and (not string_to_code)
+        (is codename )and (cursor needed) and (not string_to_string)
+        
+"""
+import logging
+import sqlite3dbm
+
+
+logging.basicConfig(filename='./toggle_name.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+#logging.getLogger('').addHandler(console)
+logging.debug('------------ start of run ------------------')
+
+
+toggle_name_DB = "./togglename.sqlite"
+
+
+language_keywords = ['and',
+                    'del',
+                    'from',
+                    'not',
+                    'while',
+                    'as',
+                    'elif',
+                    'global',
+                    'or',
+                    'with',
+                    'assert',
+                    'else',
+                    'if',
+                    'pass',
+                    'yield',
+                    'break',
+                    'except',
+                    'import',
+                    'print',
+                    'class',
+                    'exec',
+                    'in',
+                    'raise',
+                    'continue',
+                    'finally',
+                    'is',
+                    'return',
+                    'def',
+                    'for',
+                    'lambda',
+                    'try',
+                    'self',
+]
+#~ foo = getregion(data, s2c, cn)
+#~ for part in foo.component():
+    #~ part.convert()
+    #~ retrline += part.present()
+#~ return retrline
+
+class sqlHandle():
+    def open(self):
+        """Opens sql DB @ self.handle"""
+        try:
+            self.handle = sqlite3dbm.dbm.open(toggle_name_DB, 'c')    
+        except sqlite3dbm.dbm, error:
+            logging.debug( "N2C %s" %(repr(error)))
+            raise
+            
+    def close(self):
+        """Closes sql DB"""
+        del self.handle
+    
+    def val_lookup(self, key):
+        """Returns the value at self.handle[key] or None"""
+        self.open()
+        val = self.handle.get(key, None)
+        logging.debug("SQL val look up: |%s|" % (val))
+        self.close()
+        return val
+    
+    def key_lookup(self, search_val):
+        """Returns the key that returns val in self.handle 
+        asumes that there is only one key that is set to val
+        If no key, returns None""" 
+        self.open()
+        key = next((key  for key, val in self.handle.items() if val == search_val), None)
+        self.close()
+        return key
+    
+    def set_match(self, key, val):
+        """Sets self.handle[key] to val.
+        deletes any other key that points to val and returns that key
+        overwrites self.handle[key]
+        """
+        conflict_key = self.key_lookup(val)
+        self.open()
+        if conflict_key:
+            self.handle.pop(conflict_key)
+        self.handle[key] = val
+        return conflict_key
+
+    def clear_data(self):
+        self.open()
+        self.handle.clear()
+        self.close()
+    
+    
+def first_word(string):
+    """Returns the string of the first word in the string,
+    end of string is denoted by whitespace, or punk"""
+    word = ""
+    for char in string:
+        if char.isspace() or not char.isalpha() and not char =="\x01":
+            break
+        else:
+            word += char
+    return word
+    
+def key_word(string):
+    """returns lenght of the keyword at the begining of a str"""
+    fw = first_word(string)
+    first_word_sans_curser = fw.replace("\x01", "")
+    for keyword in language_keywords:
+        if first_word_sans_curser == keyword:
+            return len(fw)
+    return 0
+
+class ToggleName():
+
+    def __init__(self,data, s2c=True, cn=True):
+        self.data = data
+        self.remainingdata = data
+        
+        #Bools
+        self.s2c = s2c #string to code
+        self.cn = cn #cursor needed
+    
+    def reasemble(self):
+        rs = ""
+        for component in self.component_list:
+            data = component.present()
+            rs += data
+        return rs + self.remainingdata
+    
+    def toggle(self):
+        self.component_list = []
+        for token in self.component():
+            if (isinstance(token, string_name) and
+                self.cn == token.has_cursor() and 
+                self.s2c == True):
+                # Early out if cn?
+                token = token.convert()
+                
+            elif (isinstance(token, code_name) and
+                self.cn == token.has_cursor() and 
+                self.s2c == False):
+                
+                token = token.convert()
+            
+            elif isinstance(token, bang_name):
+                token = token.matchname(self.s2c)
+            
+            self.component_list.append(token)
+        return self.component_list
+            
+        
+    def component(self):
+        """convert data into indiviual data types"""
+        #Doesn't it makes more sence for the s2c and cn requrements apear here?
+        test_list = [self.q_not_Name,
+                 self.q_stringname,
+                 self.q_codename,]
+
+
+        while self.remainingdata:
+            for i in test_list:
+                objname = i()
+                if objname:
+                    yield objname
+                    break
+                    
+        return
+                
+
+    def q_not_Name(self):
+        """Returns not_name Object and updates self.remaningData
+        otherwise returns None"""
+        #Insepts self.data for n things
+        #Languare keywords
+        #untouchables, (quote strings & comments)
+        #whiteSpace
+        #puncuation (not inculding '_' or '\x01' or #)
+        #
+        #Ignores 
+        
+        def white_space(string):
+            """Returns length of whitespace type in the start of the string"""
+            length = 0
+            for character in string:
+                if character.isspace():
+                    length += 1
+                else: break
+            return length
+            
+
+            
+        def punk(string):
+            """returns the lenght of puncuation at the beggining of str"""
+            length = 0
+            for char in string:
+                if char.isalnum() or char in ("_", "\x01", "#", "'", '"', '"""') or char.isspace():
+                    break
+                length += 1
+            return length
+        
+        def quote_string(string):
+            """Returns the lenght of quoteString at the begginging of str"""
+            
+            qs = ('"""', "'", '"')
+            q = None
+            for quote in qs:
+                if string.startswith(quote):
+                    q = quote
+                    break
+            if q == None: return 0
+            #~ logging.debug("NN qs dectected|%s|"%string)
+            index = len(q)
+            end_index = len(string)
+            while end_index > index:
+                if string[index:].startswith(q):
+                    return index+len(q) 
+                index += 1
+            return end_index
+        
+        def hash_comment(string):
+            """Returns the lenght of the comment at the beggining of string"""
+            if not string.startswith("#"):
+                return 0
+            nl_index = string.find("\n")
+            if nl_index == -1:
+                return len(string)
+            else:
+                return nl_index
+             
+            
+        not_name_test_list = [hash_comment,
+                              white_space,
+                              key_word,
+                              quote_string,
+                              punk,
+                              ]
+        
+        length = 0
+        flag = True
+        while flag:
+            flag = False
+            for test in not_name_test_list:
+                l = test(self.remainingdata[length:])
+                if l:
+                    length += l
+                    flag = True
+                    break
+        
+        if length == 0:
+            return None        
+        
+        not_Name_obj = not_name(self.remainingdata[:length])
+        self.remainingdata = self.remainingdata[length:]
+        return not_Name_obj
+    
+    
+    
+    
+    def q_codename(self):
+        """Returns codename componendt from the front of self.remaining data
+        assumes that self.reaningdata does NOT start with a keyword"""     
+        length = 0
+        for char in self.remainingdata:
+            if not char.isalnum() and char not in ("\x01", "_"):
+                break
+            length += 1
+        
+        if length == 0: return None
+                            
+        bn = self.q_bang_name(length)
+        if bn: return bn
+        
+        
+        data = self.remainingdata[:length]
+        self.remainingdata = self.remainingdata[length:]
+        
+        return code_name(data)
+        
+    def q_bang_name(self, start_index):
+        """Returns bangobject
+        start_index = int # place in self.remaining data that is looked at
+        """
+        if self.remainingdata[start_index:].startswith("!!"):
+            bang_offset = 2 
+            
+        elif self.remainingdata[start_index:].startswith("!\x01!"):
+            bang_offset = 3
+        else:
+            return 0
+            
+        pre_bang_str = self.remainingdata[:start_index + bang_offset]
+        self.remainingdata = self.remainingdata[start_index + bang_offset:]
+        cd_data = self.q_codename().data
+        return bang_name(pre_bang_str + cd_data)
+    
+    def q_stringname(self):
+        
+        name_string = ""
+        #~ cursor = False
+        if self.remainingdata[0].isdigit():
+            return None
+        for char in self.remainingdata:
+            if char == "\x01":
+                #~ name_string = "\x01" + name_string
+                pass
+                #~ continue
+            
+            elif char == " ":
+                #if the next char is not alunum or is a keyword
+                #it marks the end of string_word
+                #except in the case where the curser is after a space
+                name_len = len(name_string)
+                next_is_keyword = key_word(self.remainingdata[name_len+1:])
+                next_is_alnum = self.remainingdata[name_len+1].isalnum()
+                next_is_cursor = self.remainingdata[name_len+1] == "\x01"
+                
+                if next_is_keyword or not next_is_alnum and not next_is_cursor:                
+                        break
+                #~ else:
+                    #~ true_string_name = True
+            
+            elif not char.isalnum():
+                if char == "_":
+                    #Bumped into a code_word, remove chars untill " " is hit
+                    name_string = name_string.rsplit(" ", 1)[0]
+                    #Then remove " "
+                    #~ name_string = name_string.rstrip("\x01")
+                    #~ name_string = name_string.rstrip(" ")
+                    print("|%s|"%name_string)
+
+                    
+                break
+            
+            name_string += char
+        
+        name_string = name_string.rstrip(" ")
+        if name_string == "" or " " not in name_string:
+            return None
+        
+        ### OLD ### Didn't work well with cursor
+        #~ length = 0
+        #~ true_string_name = False
+        #~ if self.remainingdata[0].isdigit():
+            #~ return None
+        #~ for char in self.remainingdata:
+            #~ if char == "\x01":
+                #~ length += 1
+                #~ continue
+            #~ #space dectect
+            #~ elif char == " ":
+                #~ #if the next char is not alunum or is a keyword
+                #~ #it marks the end of string_word
+                #~ #except in the case where the curser is after a space
+                #~ if (key_word(self.remainingdata[length+1:])
+                    #~ or not self.remainingdata[length+1].isalnum()
+                    #~ and self.remainingdata[length+1] != "\x01"):
+                        #~ break
+                #~ else:
+                    #~ true_string_name = True
+            #~ elif not char.isalnum():
+                #~ break
+            #~ length += 1
+        #~ 
+        #~ if length == 0 or not true_string_name:
+            #~ return None
+        ###
+        
+        
+        name_len = len(name_string)
+        #bang bang lookup
+        bb = self.q_bang_name(name_len)
+        if bb: return bb
+        
+        #intisigate string_name 
+        sn = string_name(self.remainingdata[:name_len])
+        #update remainingdata
+        self.remainingdata = self.remainingdata[name_len:]
+        
+        return sn
+                
+
+        
+class component_Parent():
+    def __init__(self, data):   
+        self.data = data
+    def present(self):
+        """Returns string form of data"""
+        return self.data.replace("\x01", "")
+    
+    def has_cursor(self):
+        return "\x01" in self.data
+        
+class string_name(component_Parent):
+    def convert(self):
+        """returns new code_name or bang_name
+        looks up sql database for partering codename, if none,
+        returns bang_name"""
+        sql = sqlHandle()
+        val = sql.val_lookup(self.data.replace("\x01", ""))
+        
+        if val == None:
+            return bang_name(self.data+"!!unknown")
+        else:
+            return code_name(val)
+        
+    
+class code_name(component_Parent):
+    def convert(self):
+        """returns new code_name or bang_name
+        looks up sql database for partering codename, if none,
+        returns bang_name"""
+        # store data in dictionary hidden inside an SQLlite database
+        if self.data == "\x01":
+            return self
+        
+        sql = sqlHandle()
+        string_n  = sql.key_lookup(self.data.replace("\x01", ""))
+        if string_n == None:
+            return bang_name("unknown!!" + self.data)
+        else:
+            return string_name(string_n)
+    
+
+class bang_name(component_Parent):
+    def matchname(self, s2c):
+        """Returns a string_name or code_name if bang_name has been
+        filled out.
+        Otherwise returns a copy of itself."""
+        # store data in dictionary hidden inside an SQLlite database
+        #What happends in the case of "  !!cod_name" ? should do testing
+        str_name , cod_name = self.data.replace("\x01", "").split("!!")
+        
+        sql = sqlHandle()
+        
+        if s2c:
+            end_type = code_name
+            goal_name = cod_name
+            other_name = str_name
+            lookup = sql.val_lookup
+        else:
+            end_type = string_name
+            goal_name = str_name
+            other_name = cod_name
+            lookup = sql.key_lookup
+            
+            
+        if "unknown" == other_name:
+            return end_type(goal_name) # replace with pass?
+        elif "unknown" == goal_name:
+            #look up if it has been updated
+            goal_name = lookup(other_name)
+            if goal_name is None:
+                return self
+                
+        else:
+            conflict = sql.set_match(str_name, cod_name)
+        return end_type(goal_name)
+
+        
+    
+        #This might be more readable. :D
+        #~ if s2c: 
+            #~ if "unknown" == str_name:
+                #~ return code_name(cod_name) # replace with pass?
+            #~ elif "unknown" == cod_name:
+                #~ #look up if it has been updated
+                #~ cod_name = sql.val_lookup(str_name)
+                #~ if cod_name is None:
+                    #~ return self
+                    #~ 
+            #~ else:
+                #~ conflict = sql.set_match(str_name, cod_name)
+            #~ return code_name(cod_name)
+
+    
+class not_name(component_Parent):
+    pass
+    
+    
---
+++ vocola/extensions/vocola_ext_togglename.py
@@ -0,0 +1,224 @@
+#!/bin/env python
+
+# system imports:
+import sys
+import string
+import re
+import time
+import sqlite3dbm
+import logging
+from optparse import OptionParser
+import traceback
+
+logging.basicConfig(filename='C:/Users/esj/Documents/toggle_name/toggle_name.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+logging.getLogger('').addHandler(console)
+logging.debug('------------ start of run ------------------')
+
+ 
+
+# local imports:
+
+from tn import *
+# description:
+#
+# togglename.py
+#
+# site control variables
+toggle_name_DB = "C:/Users/esj/Documents/toggle_name/togglename.sqlite"
+
+#Commandline processing
+######### helper functions ##########
+def commandline():
+    """process commandline"""
+        
+    # get user data from command line
+    component_list = []
+
+    usage = "I'm sorry Dave, I can't do that."
+    parser = OptionParser(usage)
+  
+    parser.add_option("-m", type="string", dest='operational_mode',
+        default = "st",
+        help="choose c[t|m] for clipboard, s[t|m] for stdin, t = test"
+                      )
+    
+    (options, parse_args) = parser.parse_args()
+
+    return (options.operational_mode)    
+
+
+class winclip:
+# clipboard access class.  handles psudo clipboard if testing
+    try: 
+        import win32clipboard
+        import win32con
+
+        def clipboard_wait_open(self):
+            while(True):
+                try:    
+                    self.win32clipboard.OpenClipboard()
+                    return
+                except Exception, error:
+                    logging.debug( "CWO %s"%(repr(error)))
+                    time.sleep(0.1)
+
+        def clipboard_get(self):
+            self.clipboard_wait_open()   
+            result = ""
+            if self.win32clipboard.IsClipboardFormatAvailable(self.win32con.CF_TEXT):
+                result=self.win32clipboard.GetClipboardData(self.win32con.CF_TEXT)
+
+            self.win32clipboard.CloseClipboard()
+            null = string.find(result, chr(0))
+            if null >0:
+                result = result[0:null]
+            return result
+
+        def clipboard_set(self,aString):
+            self.clipboard_wait_open()
+            self.win32clipboard.EmptyClipboard()
+            self.win32clipboard.SetClipboardData(self.win32con.CF_TEXT,aString)
+            self.win32clipboard.CloseClipboard()
+            return
+
+    except ImportError:
+        fake_clip = None
+
+        def clipboard_wait_open(self):
+            return True
+
+        def clipboard_get(self):
+            return self.fake_clip
+
+        def clipboard_set(self, aString):
+            self.fake_clip=aString
+            return True
+
+# vocola interfaces
+# Vocola function: toggle.name
+def vc_toggle_name():
+    logging.debug("VTN start 0")
+    try:
+        clipboard_instance = winclip()  
+        clipboard_string = clipboard_instance.clipboard_get()    
+        #logging.debug( "clip result = |%s|" % result
+        if clipboard_string:
+            logging.debug("VTN start 1 %s" % clipboard_string)
+            tn = ToggleName(clipboard_string,s2c=True,cn=True)
+            tn.toggle()
+            result = tn.reasemble()
+            
+            # place back in the clipboard
+            clipboard_instance.clipboard_set(result)
+            logging.debug( "result = |%s|" % result)
+            # logging.Debux("VTN start 2 %s" %repr(ignore_data))
+    except Exception, error:
+        logging.debug( "VTN %s" %(repr(error)))
+        traceback_string = traceback.format_exc()
+        logging.debug( "VTN TB %s" % traceback_string)
+
+    return ""
+
+# Vocola function: match.name
+def vc_match_name():
+    logging.debug("VMN start")
+    try:
+        tni = tn()
+        result = tni.clip_instance.clipboard_get()
+        logging.debug( "clip result = |%s|" % result)
+        if result:
+            (left_code, code_name, right_code) = tni.match_name(result)
+            tni.clip_instance.clipboard_set(left_code+code_name+right_code)
+    except Exception, error:
+        logging.debug( "VMN %s" %(repr(error)))
+        traceback_string = traceback.format_exc()
+        logging.debug( "VMN TB %s" % traceback_string)
+
+    return ""
+
+
+def stdio_toggle_name():
+    logging.debug("STN start 0")
+    try:
+        tni = tn()
+        logging.debug("STN start 1")
+        string = sys.stdin.read()
+        (left_code, code_name, right_code) = tni.toggle_name(string)
+        # place back in the clipboard
+        #logging.debug( "result = |%s|" % plain_name
+        sys.stdio.write( left_code+code_name+right_code)
+        logging.debug("STN start 2 %s" %repr(ignore_data))
+    except Exception, error:
+        logging.debug( "STN %s" %(repr(error)))
+
+    return ""
+
+#
+def stdio_match_name():
+    logging.debug("SMN start")
+    try:
+        logging.debug( "SMN stdio result = |%s|" % result)
+        tni = tn()
+        string = sys.stdin.read()
+        ignore_data = tni.match_name(string)
+        sys.stdio.write(left_code + code_name + right_code)
+    except Exception, error:
+        logging.debug( "SMN %s" %(repr(error)))
+
+    return ""
+
+def tests():
+    
+    #Unit test is kind of weird. Put test data into the clipboard then
+    #run the program and look at the results in the clipboard. Yeah,
+    #kind of weird.
+    test_set = {        
+        "not very simple \x01counter = 1234+ simple string": "smplCntr = 1234+ simple string",
+        "not very sim\x01ple counter = 1234+ simple string": "smplCntr = 1234+ simple string",
+        "\x01simple counter = 1234+ simple string": "smplCntr = 1234+ simple string",
+        "simple counter = 1234+ sim\x01ple string": "simple counter = 1234+ smplStr",
+        "simple counter = 1234+ simple\x01 string": "simple counter = 1234+ smplStr",
+        "simple counter = 1234+ simple string\x01": "simple counter = 1234+ smplStr",
+        }
+
+    seed_set = {        
+        "not very simple counter": "NsmplCntr",
+        "simple string": "smplStr",
+        "simple counter": "simpCnt",
+        }
+    seed_fill = {        
+        "not very simple counter!!unknown": "NsmplCntr",
+        "simple string!!unknown": "smplStr",
+        "simple counter!!unknown": "simpCnt",
+        }
+    tni = tn()
+    
+    if False:
+        logging.debug("single item test")
+        (j,k) = test_set.items()[4]
+        tni.test_case(j,k)
+    else:
+        logging.debug("multi item test")
+        for j,k in test_set.items():
+            tni.test_case(j,k)
+
+if '__main__'==__name__ :
+   
+    mode = commandline()
+    if mode == "t":
+        tests()
+    elif mode == "ct": 
+        vc_toggle_name()
+
+    elif mode == "cm": 
+        vc_match_name()
+
+    elif mode == "st": 
+        stdio_toggle_name()
+        
+    elif mode == "sm": 
+        stdio_match_name()    
