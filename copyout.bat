24f0d83ebf82e073dc2bd4b310e649eadfe120b4
Eric S. Johansson <ynotlayabout@gmail.com>
2013-05-04 15:45:49 -0400

    copy sync tools

---
+++ copyout.bat
@@ -0,0 +1,4 @@
+cd %HOMEPATH%\Documents\GitHub\togglename\vocola\extensions 
+copy /y *.* c:\natlink\natlink\vocola\extensions
+cd %HOMEPATH%\Documents\GitHub\togglename\vocola\macros
+copy *.vcl %HOMEPATH%\Documents\vocola_user
