c5b1fe03623bbecc49622982614402831d7b8ecb
Tonis FR Piip <Tonis@eggo.org>
2013-07-16 16:26:23 +0300

    Revert "tn-lisp"
    This reverts commit c60409fdc238a9af531bc108b2196aea07c16f61.

--- emacs/tn_support.el
+++ emacs/tn_support.el
@@ -1,145 +1,28 @@
-;; support file for toggle name functions when interacting with Emacs environment
-;;; --------------------------------------------
-;Narrowing
-(defun narrow-to-region-in-other-window (start end)
-  "Narrow region in another window"
-  (interactive "r")
-  (deactivate-mark)
-  (let ((buf (clone-indirect-buffer nil nil)))
-    (with-current-buffer buf
-      (narrow-to-region start end))
-      (switch-to-buffer-other-window buf))) 
+;; support file for toggle name functions when interacting with Emacs environmen
+t.
+
+savemp():= {ctrl+x}r{space}z{ctrl+x}{ctrl+x}{ctrl+x}r{space}a;
+
+restoremp() := {ctrl+x}rjz{ctrl+shift+2}{ctrl+x}rja;
 
 
-(defun narrow-to-region-in-other-frame (start end)
-  "Narrow region in another frame"
-  (interactive "r")
-  (deactivate-mark)
-  (let ((buf (clone-indirect-buffer nil nil)))
-    (with-current-buffer buf
-      (narrow-to-region start end))
-      (switch-to-buffer-other-frame buf))
-) 
-;;;-------------------------
-; emacs.vcl macro traslates
-
-(defun tn-class-pre () (interactive)
-  "Narrow class in another frame kill all"
-  (interactive)
-  (deactivate-mark)
-  (let ((buf (clone-indirect-buffer nil nil)))
-    (with-current-buffer buf
-      (narrow-to-region (py-beginning-of-class-position) (py-end-of-class-position))
-      (insert "\C-a")
-      (kill-region (point-min) (point-max))
-      )
-    (switch-to-buffer-other-frame buf))
-) 
-
-(defun tn-def-pre () (interactive)
-  "Narrow class in another frame kill all"
-  (interactive)
-  (deactivate-mark)
-  (let ((buf (clone-indirect-buffer nil nil)))
-    (with-current-buffer buf
-      (narrow-to-region (py-beginning-of-def-position) (py-end-of-def-position))
-      (insert "\C-a")
-      (kill-region (point-min) (point-max))
-      )
-    (switch-to-buffer-other-frame buf))
-) 
-
-(defun tn-fix-unknown-pre () (interactive)
-  "kill whole region"
-  (kill-region (point-min) (point-max))
+(defun tncleanup (
+		  (exchange-point-and-mark)
+		  ;; search forward for cursor marker
+		  (search-forward "\C-a")
+		  (delete-backward-char 1)
+		  )
 )
 
-(defun tn-post () (interactive)
-  "retrive region from kill buffer, look for cursor marker"
-  (yank)
-  (exchange-point-and-mark)
-  (search-forward "\C-a" nil t 1)
-  (delete-backward-char 1)
-  ;(if ( equal (point) (point-max)) ; and if indirect buffer
-  ;   (message "at end of buff");tn-kill-frame-and-narrowed-buffer) ; kill buffer & frame. ;should have the narrowed buffer name have a char in it.
-  ; )
-)
-
-(defun tn-kill-frame-and-narrowed-buffer () (interactive)
-  "kills the narrwed buffer and deletes the frame"
-  (kill-buffer)
-  (delete-frame) ;
-)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(defun toggle-region (start end)
-  "selected region will be toggleed and replaced"
-  (interactive "r")
-  (shell-command-on-region (start) (end) "python ~/Documents/GitHub/togglename/vocola/extensions/vocola_ext_togglename.py -mst" t)
-)
-
-(defun toggle-buffer () (interactive)
-  "toggle the whole buffer"
-  (toggle-region (point-min) (point-max))
-)
-
-;(defun tn-toggle-def-or-class () (interactive)
-;  ((narrow-frame-def-or-class)
-;   
-;   ...
-;;;-------------------------
-; emacs.vcl macro traslate
-
-(defun tncleanup () (interactive)
- "Search forward, delete cursor mark. Return t if point == point-end"
- (yank)
- (exchange-point-and-mark)
-  ;; search forward for cursor marker
- (search-forward "\C-a")
- (delete-backward-char 1)
- (if ( equal (point) (point-max)) ; and if indirect buffer
-     t ; kill buffer & frame. ;should have the narrowed buffer name have a char in it.
-   )
-)
-
+(defun toggle-statement-pre (
+			     (py-kill-statement)
+			     (yank)  ;; define region
+			     ;;(narrow-to-region    
+			     ;; (region-beginning) 
+			     ;; (region-end))
+			     ;; ends here for return to vocola
+			     ;; toggle.name(1,0) 
+(defun toggle-statement-post (
+			      (exchange-point-and-mark)
+			      (yank) ;; overwrites active region
+			      (exchange-point-and-mark) ;; activate it again
---
+++ vocola/extensions/fix_unknown_tests.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  fix_unknown_tests.py
+#  
+#  Copyright 2013 Tonis <Tonis@PLUTO>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+from tn import *
+
+cursor = "\x01" 
+
+tests = [ ("", cursor),
+		  (cursor, cursor),
+		  ("class !!unknown():", "class "+cursor+"():"),
+		  ("foo bar!!unknown", "foo bar!!"+cursor),
+		  ("unknown!!foo", cursor + "!!foo"),
+		  ("unkno"+cursor+"wn!!foo",cursor + "!!foo"),
+		  ("class !!unknown(foo bar!!unknown):","class "+cursor +"(foo bar!!unknown):"),
+		    
+		]
+for test, result in tests:
+	
+    tn = ToggleName(test)
+    tn.fix_unknown()
+    tr = tn.reasemble()
+    if tr == result:
+        print("Test passed: " + test)
+    else:
+        print("Test failed: Beginning data dump")
+        print("Test:", test)
+        print("Correct result: |%s|"%result)
+        print("Actual result: |%s|"%tr)
+        print("----")
+        for i in tn.get_parsed_data():
+            print("|%s|"%i.data)
+            print(i.__class__.__name__)
+            print("----")
+        print("")
