d96c9e5a5ea3c47d90fdbc35ef77103f625fb194
Tonis <tonis@Mercury.(none)>
2013-09-13 20:38:38 +0300

    Bonenames added

--- vocola/extensions/tn.py
+++ vocola/extensions/tn.py
@@ -16,7 +16,97 @@
         
         (is codename )and (not cursor needed) and (not string_to_code)
         (is codename )and (cursor needed) and (not string_to_string)
-        
+
+
+----------------------------
+
+Toggle Box: Programing by speach
+A collection of editor tools for easy to dictate and read code formaters.
+
+
+The fundimental problem that arises when dictating code is syntaxs.
+It is slow and annoying to dictate every open_square_bracket close_square_bracket
+every under_score and every exclamation point equals.
+
+This coupled with the problems that dictation software has with English syntaxs,
+such as capatlasation and using spaces in between words rather then underscores;
+makes coding by speach a complex time consuming task.
+
+Toggle Box is the solution.
+
+String-names solve the problem of having dictated variables.
+
+A string-name is, simply, an illgal variable name that is in plain english such as:
+    a long river
+    input stack
+    parrent node
+    temp value
+    error flag
+These would normaly throw an error when being assigned to. 
+When coding we tend to use the same variable names more then one time, and in more
+then one scope or project. So we pair each string-name to a code-name. A python legal 
+variable name, such as:
+    aLongRiver
+    input_stack
+    parrentNode
+    tmpvar
+    ERROR_FLAG
+
+After dictating the code, simply call Toggle Box's apropriate command and all string-name
+will automaticly converted into code-name.
+
+Here is an example of pre toggle and post toggle code:
+
+class Train():
+    def __init__(self):
+        self.is moveing = True
+
+    def running():
+        while self.is moving:
+            self.blow horn()
+            if distance to next city < self.stopping distance:
+                self.slow down()
+
+class Train():
+    def __init__(self):
+        self.is_moveing = True
+
+    def running():
+        while self.is_moving:
+            self.blow_horn()
+            if cityDist < self.stopDist:
+                self.reduseSpeed()
+
+If however a string-name does not have a pair mached to it, it will be suffixed by "!!unknown"
+when this happens, one of ToggleBox's fix commands should be called, that will repalce the "unknown"
+with the curser, for quick typing of the desired code-name. Then Fix Next should be called,
+all following accurances of the string-name will be replaced by the newly matched code-name.
+
+There are also methods avalble to revert code-names back into string-name, and match code-names to string-names.
+
+-----------------------------------------------
+
+Sceloton commands and bone-names.
+Quck generation of symbol heavy syntaxs.
+
+Another problem that accurs when dictating code are symbol syntaxs.
+
+Skeloton commands generate the empty shell of a regular coding pattern such as deffining new methods. 
+Using that example the 'new method' skellton command would generate:
+
+          
+new method:count backwards arg list:up to:
+    for count in range arg list:0, up to, -1:
+        print count
+
+new method:count backwards arg list:up to:
+    while up to > 0:
+        print up to
+        up to += 1
+
+
+
+
 """
 import logging
 import sqlite3dbm
@@ -94,7 +184,9 @@ class sqlHandle():
     def key_lookup(self, search_val):
         """Returns the key that returns val in self.handle 
         asumes that there is only one key that is set to val
-        If no key, returns None""" 
+        If no key, returns None"""
+        # Rather then searing though all string-code pairs, when setting new string-code pairs, also set a code-string pair.
+        # there shouldn't be any conflicts... :\
         self.open()
         key = next((key  for key, val in self.handle.items() if val == search_val), None)
         logging.debug("SQL key look up with val |%s|: |%s|" % (search_val, key))
@@ -148,6 +240,7 @@ class ToggleName():
         
         
         self.component_list = []
+        self.nesting_stack = [self.component_list]
         self.component_count = {} 
     
     def reasemble(self):
@@ -164,8 +257,7 @@ class ToggleName():
         """Togglename command
         s2c = string to code. The direction of toggle. Toggle/flip
         """
-        
-        #Maybe do a if not bool(self.get_parsed_data()): self.goto_start() ?
+                #Maybe do a if not bool(self.get_parsed_data()): self.goto_start() ?
         #I don't think there's a time that we would call toggle twice in the same isntance, but for the furture maybe?
         
         cursor_found = False
@@ -176,18 +268,17 @@ class ToggleName():
             if (isinstance(token, string_name) and 
                 s2c == True and
                 cn == cursor_found):
-                token = token.convert()
+                token.convert()
                 
             elif (isinstance(token, code_name) and 
                 s2c == False and
                 cn == cursor_found):
-                token = token.convert()
+                token.convert()
             
             elif (isinstance(token, bang_name)and
                 cn == cursor_found):
-                token = token.matchname(s2c)
-            
-            self.component_list.append(token)
+                token.matchname(s2c)
+                
             if cursor_found:
                 break
         return 
@@ -206,7 +297,6 @@ class ToggleName():
         self.remainingdata = self.data.replace(CURSOR_MARKER,"")
         
         for part in self.component():
-            self.component_list.append(part)
             if isinstance(part, bang_name):
                 if part.fix_unknown():
                     break
@@ -221,22 +311,59 @@ class ToggleName():
     def component(self):
         """convert data into indiviual data types"""
                 
-        test_list = [self.q_not_Name, #looks for punkucations + whitespace + comments + quotes
-                    self.q_bang_name, #handles floating !!'s
-                    self.q_stringname,
-                    self.q_codename,
+        test_list = [self.q_bone_name, # looks for Bone-names, must be done before q_not_Name
+                     self.q_not_Name, #looks for punkucations + whitespace + comments + quotes
+                     self.q_bang_name, #handles floating !!'s
+                     self.q_stringname,
+                     self.q_codename,
                     ]
 
-
+        previous_token = None
         while self.remainingdata:
             for i in test_list:
-                objname = i()
-                if objname:
-                    self.count(objname)
-                    yield objname
+                token = i()
+                if token:
+                    
+                    if self.token_is_nesting_closeur_trigger(token):
+                        self.reduse_nesting()
+                    if isinstance(token, Bone_Name):
+                        self.nest_token(token)
+                    else:
+                        self.add_token(token)
+                    self.count(token)
+                    if previous_token is None:
+                        previous_token, token = token, None
+                        break
+                    yield previous_token
+                    previous_token, token = token, None
                     break
+        yield previous_token
                     
         return
+
+    def token_is_nesting_closeur_trigger(self, token):
+        if len(self.nesting_stack) > 1:
+            return self.nesting_stack[-1].is_closing_trigger(token)
+        else:
+            return False
+        
+
+    def add_token(self, token):
+        if len(self.nesting_stack) > 1:
+            self.nesting_stack[-1].append(token)
+        else:
+            self.component_list.append(token)
+
+    def nest_token(self, token):
+        self.add_token(token)
+        self.nesting_stack.append(token)
+
+    def reduse_nesting(self):
+        if len(self.nesting_stack) == 1:
+            return
+        else:
+            self.nesting_stack.pop()
+
                 
     def count(self, component):
         """increments the compnent types count by 1 in self.component_count"""
@@ -391,9 +518,6 @@ class ToggleName():
             post_bang_string = post_bang_data.present()
         return bang_name(pre_bang_str + post_bang_string)
     
-    def q_unknown(self):
-        """returns unknown object"""
-        pass
     
     def q_stringname(self):
         
@@ -413,14 +537,14 @@ class ToggleName():
                 #except in the case where the curser is after a space
                 name_len = len(name_string)
                 next_is_keyword = key_word(self.remainingdata[name_len+1:])
-                next_is_alnum = self.remainingdata[name_len+1].isalnum()
-                next_is_cursor = self.remainingdata[name_len+1] == CURSOR_MARKER
+                try:
+                    next_is_alnum = self.remainingdata[name_len+1].isalnum()
+                    next_is_cursor = self.remainingdata[name_len+1] == CURSOR_MARKER
+                except IndexError:
+                    return None
                 
-                if next_is_keyword or not next_is_alnum and not next_is_cursor:                
+                if next_is_keyword or not next_is_alnum and not next_is_cursor:
                         break
-                #~ else:
-                    #~ true_string_name = True
-            
             elif not char.isalnum():
                 if char == "_":
                     #Bumped into a code_word, remove chars untill " " is hit
@@ -452,9 +576,45 @@ class ToggleName():
         
         return sn
                 
+    def q_bone_name(self):
+        """
+        returns a bone-name token, if self.remaining_data starts with a vaild bone_name, else returns None
+        also updates self.remainingdata
 
+        will not nest the colon name, only detect.
+        """
+        token = None
+        if not self.remainingdata.startswith("::"):
+            return token
+        #Possible chance for bone name.
+
+        #inspect data of last token, to see if it is indeed a valid Bone-name
+        try:
+            previous_token = self.nesting_stack[-1][-1] #is there a chance that a closed nesting group would cause bugs? :\
+        except IndexError:
+            previous_token = None
         
-class component_Parent():
+        if not isinstance(previous_token, (string_name, code_name, not_name)):
+            return token
+        for valid_name in Bone_Name.get_valid_names():
+            if previous_token.data.endswith(valid_name):
+                token = Bone_Name.new(valid_name)
+                #nesting it sould be done at a differnt level
+                #self.nest_token(token)
+                previous_token.data = previous_token.data.rpartition(valid_name)[0].strip()
+                #remove the "::"
+                self.remainingdata = self.remainingdata[2b:]
+                return token
+        
+        
+class component_Parent(object):
+    
+    def __str__(self,):
+        return "<%s |%s|>"%(self.__class__.__name__, self.data)
+
+    def __repr__(self,):
+        return self.__str__()
+
     def __init__(self, data, had_cursor = False):
         if had_cursor == True:
             self.had_cursor = True
@@ -473,6 +633,12 @@ class component_Parent():
         
     def has_cursor(self):
         return self.had_cursor
+
+    def convert_to_notname(self):
+        self.__class__ = not_name
+    
+    def convert_to_nullname(self):
+        self.__class__ = null_name
         
 class string_name(component_Parent):
     def convert(self):
@@ -483,9 +649,11 @@ class string_name(component_Parent):
         val = sql.val_lookup(self.data)
         
         if val == None:
-            return bang_name(self.data+"!!unknown", had_cursor = self.had_cursor)
+            self.__class__ =  bang_name
+            self.__init__(self.data+"!!unknown", had_cursor = self.had_cursor)
         else:
-            return code_name(val, had_cursor = self.had_cursor)
+            self.__class__ = code_name
+            self.__init__(val, had_cursor = self.had_cursor)
         
     
 class code_name(component_Parent):
@@ -500,9 +668,11 @@ class code_name(component_Parent):
         sql = sqlHandle()
         string_n  = sql.key_lookup(self.data.replace(CURSOR_MARKER, ""))
         if string_n == None:
-            return bang_name("unknown!!" + self.data, had_cursor = self.had_cursor)
+            self.__class__ = bang_name
+            self.__inti__("unknown!!" + self.data, had_cursor = self.had_cursor)
         else:
-            return string_name(string_n, had_cursor = self.had_cursor)
+            self.__class__ = string_name
+            self.__init__(string_n, had_cursor = self.had_cursor)
     
 
 class bang_name(component_Parent):
@@ -541,7 +711,10 @@ class bang_name(component_Parent):
                 
         else:
             conflict = sql.set_match(str_name, cod_name)
-        return end_type(goal_name, had_cursor = self.had_cursor)
+
+        
+        self.__class__ = end_type
+        self.__init__(goal_name, had_cursor = self.had_cursor)
 
         
     
@@ -585,7 +758,103 @@ class bang_name(component_Parent):
             self.data = left + "!!" + right
         
         return True
+
 class not_name(component_Parent):
     pass
+
+class null_name(component_Parent):
+    """A null name,
+    if a token is converted to this class, will essentally be deleted
+    """
+    def present(self):
+        return ""
+
+class Bone_Name(component_Parent):
+    """
+    """
+
+    def __init__(self,):
+        component_Parent.__init__(self, self.__class__.__name__.lower().replace("_", " ",) + "::")
+        self.nesting_list = []
+        self.prefix = ""
+        self.suffix = ""
+        self.had_cursor = False
+        self.trigger = "\n"
+        
+#        self.set_bones("(",")") # default bones, 
+                                # to be used to testing only, 
+                                # subclasses should use this 
+                                # method to asign what it formats
+                                # (prefix, sufix)
+    @staticmethod    
+    def get_valid_names():
+        
+        valid_names = {sub_class.__name__.lower().replace("_", " "):sub_class for sub_class in Bone_Name.__subclasses__()}
+        #valid_names = {"arg list": ""}
+        return valid_names
     
+    @staticmethod
+    def new(boneString):
+        """Returns the correct subclass instance of boneString, or None if there is no subclass of with that name"""
+        subclass = Bone_Name.get_valid_names().get(boneString, None)
+        if subclass is not None:
+            return subclass()
+        else:
+            return None
+
+    def __str__(self):
+        return "<%s |%s|>"%(self.__class__.__name__, self.nesting_list)
+    def __repr__(self):
+        return self.__str__()
+
+    def __getitem__(self, key):
+        return self.nesting_list[key]
     
+    def append(self, token):
+        self.nesting_list.append(token)
+    
+    def set_bones(self, prefix, suffix):
+        self.prefix = prefix
+        self.suffix = suffix
+
+
+    def present(self,):
+        val =  [self.prefix]
+        #dealing with the seperators for dictionars looks to be tricky,
+        #sepcount = 0
+        for t in self.nesting_list:
+            val.append(t.present())
+            #val.append(self.sep[sepcount])
+            #selcount = (sepcount + 1)%len(sepcount)
+        val.append(self.suffix)
+        return "".join(val)
+
+    def is_closing_trigger(self, token):
+        if isinstance(token, not_name):
+            if self.trigger in token.data:
+                return True
+        return False
+            
+
+class Arg_list(Bone_Name):
+    def __init__(self):
+        Bone_Name.__init__(self)
+        self.set_bones("(",")")
+    
+class New_tuple(Arg_list, Bone_Name):
+    pass
+
+class Quoted_tripple(Bone_Name):
+    def __init__(self):
+        Bone_Name.__init__(self)
+        self.set_bones('"""', '"""')
+        self.trigger = Quoted_tripple
+    def is_closing_trigger(self, token):
+        # will not close being q_bonename will not go
+        # because the previous token will be turned into 
+        # notnames
+        if isinstance(token, Quoted_tripple):
+            token.convert_to_nullname()
+            return True
+        token.convert_to_notname()
+        return False
